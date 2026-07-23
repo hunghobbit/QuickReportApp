0f7654e3abbcb29a5b99c68606259284d91f51a7
hunghobbit <50900306+hunghobbit@users.noreply.github.com>
2026-07-24 03:13:02 +0700

    Update vite.config.js

--- clients/vite.config.js
+++ clients/vite.config.js
@@ -4,15 +4,23 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3001,
+  // server: {
+  //   port: 3001,
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:3000',
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
+   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://quick-report-api.onrender.com',
         changeOrigin: true,
-      },
-    },
-  },
+      }
+    }
+  }
   resolve : {
     alias: {
       "@" : path.resolve(__dirname, "./src/"),
